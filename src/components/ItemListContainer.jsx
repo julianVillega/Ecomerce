cf95beb0ceec234ce60e005bb8494c8c2b8eb46b
julianVillega <julianvillega9@gmail.com>
2024-07-28 11:57:40 -0300

    reescribo el codigo para colocar los productos en rows y columns de bootstrap

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,15 +2,43 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { Product } from './Product';
+import { useEffect, useState } from 'react';
+
+import MOCK_DATA from "../assets/json/MOCK_DATA"
+
 export default function ItemListContainer ({greeting}){
+    const [productos, setProductos] = useState([])
+
+    useEffect(()=>{
+        new Promise((resolve, reject)=>{
+            setTimeout(() => resolve(MOCK_DATA), 1000);
+        })
+        .then((response) => setProductos(response))
+    },[])
+
+    const rows = []
+    for(let i = 0; i< productos.length; i = i + 3){
+        let terna = productos.slice(i, i + 3);
+        terna = terna.map((producto) => {             
+            return(
+            <Col xs={3} key={producto.id}>
+                <Product name={producto.name} description={producto.description} image={producto.image}/>
+            </Col>)
+        })
+        if(terna.length < 3){
+            let numberOfDummyCols = 3 - terna.length;
+            terna = [...terna, ...Array(numberOfDummyCols).fill(null).map((_, index)=><Col xs={3} key={`dummy-${index}`}></Col>)]
+        }
+        rows.push(
+            <Row key={i} className='my-5 justify-content-evenly'>{terna}</Row>
+        )
+    }
+
     return(
         <>
-        <Container className='hero-container'>
-            <Row className='align-items-center'>
-                <Col className='text-center'>
-                    <h1>{greeting}</h1>
-                </Col>
-            </Row>
+        <Container  className='gy-5'>
+            {rows}
         </Container>
         </>
     )
