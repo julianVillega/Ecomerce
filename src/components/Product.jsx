1d72521b1d214996b5d97196fc686d0235822db6
julianVillega <julianvillega9@gmail.com>
2024-07-29 15:24:08 -0300

    hago que todas las imagenes tengan la misma altura

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,12 +3,14 @@ import Card from 'react-bootstrap/Card';
 
 export const Product = ({name, description, image}) => {
     return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={image} />
+        <Card style={{ width: '18rem'}}>
+            <div className='d-flex justify-content-center product-image-div'>                
+                <Card.Img variant="top" src={image} className='product-image'/>                
+            </div>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
-                    {description}
+                    {description.slice(0, 50)}
                 </Card.Text>
                 <Button variant="primary">Detalles</Button>
             </Card.Body>
