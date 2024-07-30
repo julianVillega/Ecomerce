f221a67191619284c0bc71cb1e7c662cb76bdaad
julianVillega <julianvillega9@gmail.com>
2024-07-30 16:27:44 -0300

    agrego spinners durante la carga de los productos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,13 +4,18 @@ import { useEffect, useState } from 'react';
 import MOCK_DATA from "../assets/json/MOCK_DATA"
 import { useParams } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
 export default function ItemListContainer (){
     const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
     const {categoryId} = useParams()
 
     useEffect(()=>{
         //simulo el uso de una API con una promesa
         new Promise((resolve, reject)=>{
+            setLoading(true)
             setTimeout(() => resolve(MOCK_DATA), 1000);
         })
         .then(
@@ -22,17 +27,29 @@ export default function ItemListContainer (){
                 else{
                     setProductos(response.filter(prod => prod.category == categoryId))
                 }
-            })
+            }).finally(() => setLoading(false));
     },[categoryId])
 
 
-    //Construo una grilla de bootstrap con los productos.
+    //creo los componentes para los productos
     const prods = productos.map((producto)=><Product key={producto.id} name={producto.name} description={producto.description} image={producto.image}/>)
+    
+    //creo spinners para mostrar durante la carga de los productos
+    const placeHolders = new Array(
+        <FontAwesomeIcon key={1} icon={faSpinner} spin className='fa-7x product-loading-spinner'/>,
+        <FontAwesomeIcon key={2} icon={faSpinner} spin className='fa-7x product-loading-spinner'/>,
+        <FontAwesomeIcon key={3} icon={faSpinner} spin className='fa-7x product-loading-spinner'/>,
+    )
+
 
     return(
         <>
-        <div className='div-productos'>
-            {prods}
+        <div className='div-productos'>            
+            {(()=>{
+                if(loading) return placeHolders
+                if(prods.length == 0) return <h2>No se encotraron productos</h2>
+                return prods
+            })()}
         </div>
         </>
     )
